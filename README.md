e0a749e09fb93ffdbf1ed36915f3c7fab9b5cfa1
mak <kadir4444@live.co.uk>
2017-03-22 14:32:50 +0000

    mak1

---
+++ README.md
@@ -0,0 +1,2 @@
+# QAmak
+# QAmak
---
+++ vagrant.java
@@ -0,0 +1,13 @@
+public class Vagrant_Test{
+	
+	public static void main (String[] args){
+		
+		int value1 = 10;
+		int value2 = 3;
+		
+		System.out.println("Hello Mak");
+		System.out.println("Answer is: " + (value1 + value2));
+	}
+	
+	
+}
